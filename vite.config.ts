dcbaf014d64c1fc91d38bf3034794b82f4aed873
mo2352266377-commits <mo2352266377@gmail.com>
2026-03-03 11:51:29 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import path from 'path';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // 关键：把 tailwindcss() 放在最前面
-  plugins: [tailwindcss(), react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    },
-  },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 });
